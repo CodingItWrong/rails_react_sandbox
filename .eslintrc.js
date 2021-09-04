b167f85782874fe5430e89e084aa8366463270b4
Josh Justice <me@codingitwrong.com>
2021-09-04 11:17:06 -0400

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  plugins: ['prettier', 'import', 'react'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  globals: {
+    document: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'import/order': ['error', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'sort-imports': [
+      'error',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+  },
+};
