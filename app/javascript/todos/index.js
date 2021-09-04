23a7b1f10e2cb424c2bd22734100b4b79957c1e6
Josh Justice <me@codingitwrong.com>
2021-09-04 08:38:50 -0400

    Display todos

--- app/javascript/todos/index.js
+++ app/javascript/todos/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import TodoList from './TodoList';
 
 export default function Todos() {
-  return <p>Todos</p>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/todos')
+      .then(response => setTodos(response.data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      <h1>Todos</h1>
+      <TodoList todos={todos} />
+    </>
+  );
 }
