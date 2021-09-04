803b2b731a522c79c923809f76fd60a161f4ba8f
Josh Justice <me@codingitwrong.com>
2021-09-04 08:35:45 -0400

    Set up todo page

---
+++ app/javascript/packs/todos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Todos from '../todos'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Todos />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
