de583d129060bf01ff5d6b89c33efe53a1c0dc0e
Josh Justice <me@codingitwrong.com>
2021-09-07 19:56:45 -0400

    Add a validation test

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'considers records without a name invalid' do
+      todo = FactoryBot.build(:todo, name: '')
+      expect(todo).not_to be_valid
+    end
+
+    it 'considers records with a name valid' do
+      todo = FactoryBot.build(:todo, name: 'my todo name')
+      expect(todo).to be_valid
+    end
+  end
 end
