1ef05d6e41cc5149de024df05071f8bb3a5bb202
Josh Justice <me@codingitwrong.com>
2021-09-07 20:16:55 -0400

    Test delete button in list

--- app/javascript/todos/TodoList.spec.js
+++ app/javascript/todos/TodoList.spec.js
@@ -1,17 +1,26 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import TodoList from './TodoList';
 
 describe('TodoList', () => {
+  const todo1 = {id: 1, name: 'Todo 1'};
+  const todo2 = {id: 2, name: 'Todo 2'};
+  const todos = [todo1, todo2];
+
   it('renders passed-in todo names', () => {
-    const todos = [
-      {id: 1, name: 'Todo 1'},
-      {id: 2, name: 'Todo 2'},
-    ];
+    render(<TodoList todos={todos} />);
 
-    const {queryByText} = render(<TodoList todos={todos} />);
+    expect(screen.queryByText('Todo 1')).not.toBeNull();
+    expect(screen.queryByText('Todo 2')).not.toBeNull();
+  });
 
-    expect(queryByText('Todo 1')).not.toBeNull();
-    expect(queryByText('Todo 2')).not.toBeNull();
+  it('calls the onDelete handler when a delete button is clicked', () => {
+    const onDelete = jest.fn();
+    render(<TodoList todos={todos} onDelete={onDelete} />);
+
+    userEvent.click(screen.getByText(`Delete ${todo1.name}`));
+
+    expect(onDelete).toHaveBeenCalledWith(todo1);
   });
 });
